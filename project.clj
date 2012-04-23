7e2b0d4592d8274ea0a6c0e14f56de4ea341a44b
Nelson Morris <nmorris@nelsonmorris.net>
2012-04-23 15:11:45 -0500

    Remove local snapshot dependency

--- project.clj
+++ project.clj
@@ -8,5 +8,14 @@
                  [environ "0.2.1"]
                  [com.cemerick/friend "0.0.7"]]
   :profiles {:test {:resource-paths ["test-resources"]
-                    :dependencies [[com.cemerick/pomegranate "0.0.12-SNAPSHOT"]
-                                   [ring "1.0.2"]]}})
+                    :dependencies
+                    [[ring/ring-jetty-adapter "1.0.1"]
+
+                     ;; TODO remove exclusions when updated
+                     ;; Fix pomegranate http deploy
+                     [com.cemerick/pomegranate "0.0.11"
+                      :exclusions
+                      [[org.apache.maven.wagon/wagon-http]
+                       [org.apache.maven.wagon/wagon-provider-api]]]
+                     [org.apache.maven.wagon/wagon-http "2.2"]
+                     [org.apache.maven.wagon/wagon-provider-api "2.2"]]}})
